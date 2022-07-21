d2fab77b685260bce46cdf5b80a1deb8e0628315
Elton Bautista <eltonbestaha@gmail.com>
2022-07-21 17:07:18 -0600

    Created function for mapping repos and display them conditionally Also wrote my function for filtering list via language Unfortunately I spent too much time dealing with a CORS error, and setting up my server in part A If I didn't exceed the time limit I would have progressed by utilizing each RepoDisplay's language and setting state to re-render the mapped components on button click.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,14 +4,19 @@ import React, { useEffect, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import './App.css';
 import RepoDisplay from './components/RepoDisplay';
+import { Repo } from '../../api/src/models/Repo';
 
 export function App() {
-  const [myRepos, setMyRepos] = useState<undefined | AxiosResponse<any, any>>();
+  const [myRepos, setMyRepos] = useState<
+    undefined | AxiosResponse<any, any> | any
+  >();
+
+  const [languageCoordinator, setLanguageCoordinator] = useState<any>([]);
 
   useEffect(() => {
     const asyncProcess = async () => {
       const myData = axios.get('http://localhost:4000/repos').then((res) => {
-        setMyRepos(res);
+        setMyRepos(res.data);
         return res;
       });
       return myData;
@@ -19,10 +24,46 @@ export function App() {
     asyncProcess();
   }, []);
 
+  const mapDisplays = (arrayToMap: Array<typeof myRepos>) => {
+    if (arrayToMap) {
+      return arrayToMap.map((repoObject, i) => {
+        return (
+          <RepoDisplay
+            key={i}
+            name={repoObject?.full_name}
+            description={repoObject.description}
+            language={repoObject.language}
+            forksCount={repoObject.forks_count}
+          />
+        );
+      });
+    }
+  };
+
+  const mappedDisplays = mapDisplays(myRepos);
+
+  function filterListViaLanguage(filterLang: string, arr: typeof myRepos) {
+    if (arr !== undefined) {
+      const noneFilteredArray: Repo[] = [];
+
+      const myFilteredArray = arr.filter(function (repo: Repo) {
+        if (repo.fork === false) {
+          // encode repo
+          return repo;
+        }
+        noneFilteredArray.push(repo);
+        return undefined;
+      });
+      setLanguageCoordinator([...myFilteredArray, ...noneFilteredArray]);
+    }
+  }
+
   return (
     <div className="App">
       <h1>My repos:</h1>
-      <div className="repository-container"></div>
+      <div className="my-repos">
+        {mappedDisplays ? mappedDisplays : 'Loading data..'}
+      </div>
     </div>
   );
 }
