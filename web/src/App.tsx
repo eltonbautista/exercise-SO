6ada5908cc69bb5f9063b2e51eba91097fb1941d
Elton Bautista <eltonbestaha@gmail.com>
2022-07-21 16:26:01 -0600

    Wrote logic for getting repository data from proxy server Commented out some ESLint due to VSC errors popping up due to older version of VSC Will uncomment before building to ensure better coding practice

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/self-closing-comp */
+import React, { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
 import './App.css';
+import RepoDisplay from './components/RepoDisplay';
 
 export function App() {
+  const [myRepos, setMyRepos] = useState<undefined | AxiosResponse<any, any>>();
+
+  useEffect(() => {
+    const asyncProcess = async () => {
+      const myData = axios.get('http://localhost:4000/repos').then((res) => {
+        setMyRepos(res);
+        return res;
+      });
+      return myData;
+    };
+    asyncProcess();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My repos:</h1>
+      <div className="repository-container"></div>
     </div>
   );
 }
